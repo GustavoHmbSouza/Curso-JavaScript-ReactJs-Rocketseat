f4c3b0efc8db6892453c5315c422a1a7e538243f
Gustavo_Matos <gustavo.matos@smn.com.br>
2019-09-05 17:01:58 -0300

    ReactJs

---
+++ curso-ReactJs-04-BusandoProdutosApi/huntweb/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './styles.css';
+import Header from './components/Header';
+
+const App = () =>
+(
+    <div className="App">
+        < Header/>
+    </div>
+);
+
+export default App;
